4906d0160337b31044e928777877ccbf84bd611c
Ahmed <ahmed_l2@yahoo.com>
2024-03-24 02:50:12 +0300

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
 #Using puppet to conenct to ssh server without password
-
-file { '/etc/ssh/sshd_config'
+file { '/etc/ssh/sshd_config':
   ensure  => present,
   content => "
     Host *
