04febc2f43113453be09671089a82fb3632217e0
Ahmed <ahmed_l2@yahoo.com>
2024-03-26 23:54:55 +0300

    5. Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,31 +1,28 @@
 # Setup Ubuntu Server with nginx
 
-exec { 'system_update':
-  command => '/usr/bin/apt-get update',
-}
-
 package { 'nginx':
   ensure  => installed,
-  require => Exec['system_update'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx']
 }
 
 file { '/etc/nginx/sites-available/default':
   ensure  => present,
   content => "
-    server {
-    listen 80;
-    location / {
-        return 200 'Hello World!';
-        add_header Content-Type text/plain;
-    }
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
-    }",
+server {
+	listen 80;
+	location / {
+		return 200 'Hello World!';
+        	add_header Content-Type text/plain;
+    	}
+
+    	location /redirect_me {
+        	return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
+    	}
+}",
   notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
