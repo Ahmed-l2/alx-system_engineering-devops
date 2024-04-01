c4f2aec9c606366c7ae731acc7a22f13cfd36d13
Ahmed <ahmed_l2@yahoo.com>
2024-04-01 23:38:11 +0300

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,18 +6,21 @@ package { 'nginx':
 
 file { '/etc/nginx/sites-available/default':
   ensure  => present,
-  content => "
-server {
-        listen 80;
-        location / {
-                return 200 'Hello World!';
-                add_header Content-Type text/plain;
-		add_header X-Served-By ${hostname};
-        }
-
-        location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
-        }
+  content => "server {
+    listen 80;
+    error_page 404 /c404.html;
+    location = / {
+        return 200 'Hello World!';
+        add_header Content-Type text/plain;
+        add_header X-Served-By ${hostname};
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
+    }
+    location = /c404 {
+        root /usr/share/nginx/html;
+        internal;
+    }
 }",
   notify  => Service['nginx'],
 }
