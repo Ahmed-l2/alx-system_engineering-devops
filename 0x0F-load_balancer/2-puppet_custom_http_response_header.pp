7fcb75e869a69bdae640fa39f1d065d18bafa355
Ahmed <ahmed_l2@yahoo.com>
2024-04-01 23:45:18 +0300

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # Setup Ubuntu Server with nginx
 
 exec { 'command':
-  command  => 'apt-get update;
-  apt-get install nginx -y;
-  echo "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" | sudo tee /etc/nginx/sites-available/default;
-  service nginx restart',
+  command  => 'apt-get update; \
+               apt-get install nginx -y; \
+               echo "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" | sudo tee /etc/nginx/sites-available/default; \
+               service nginx restart',
   provider => shell,
 }
