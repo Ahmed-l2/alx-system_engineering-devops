9b4cfa5dd359fe838bfeddaefe0ccbed5ad7a9a1
Ahmed <ahmed_l2@yahoo.com>
2024-04-23 01:19:32 +0300

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -22,7 +22,7 @@ def fetch_data(id):
                 task_list.append('\t ' + key['title'])
                 done_task += 1
 
-    print("Employee {} is done with task({}/{}):".format(u_list["name"],
+    print("Employee {} is done with tasks({}/{}):".format(u_list["name"],
                                                          done_task, max_task))
     print('\n'.join(task_list))
 
