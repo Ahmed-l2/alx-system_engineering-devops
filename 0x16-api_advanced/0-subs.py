cece1a434c5d8e10e711665551211e81f1f60c6b
Ahmed <ahmed_l2@yahoo.com>
2024-05-07 20:29:27 +0300

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,6 @@ import requests
 def number_of_subscribers(subreddit):
     """Fetches subreddit subcount"""
 
-    if not subreddit or type(subreddit) is not str:
-        return 0
-
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     header = {'User-Agent': 'Mozilla/5.0'}
@@ -18,6 +15,7 @@ def number_of_subscribers(subreddit):
         return response.json()['data']['subscribers']
     else:
         return 0
+
 
 if __name__ == "__main__":
     number_of_subscribers(argv[1])
