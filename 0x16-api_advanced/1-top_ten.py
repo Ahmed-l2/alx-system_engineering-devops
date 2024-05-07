96445b77f5d77d6b05849ada62c5be674df8bd46
Ahmed <ahmed_l2@yahoo.com>
2024-05-07 19:37:58 +0300

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script that fetches Number of subscribers for a subreddit"""
+import requests
+import sys
+
+def top_ten(subreddit):
+    """Fetches subreddit subcount"""
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    header = {'User-Agent': 'Chrome'}
+    limit = {'limit': 10}
+    response = requests.get(url, headers=header, params=limit, allow_redirects=False)
+
+    if response.status_code == 200:
+        for post in response.json()['data']['children']:
+            print(post['data']['title'])
+    else:
+        return 0
+
+if __name__ == "__main__":
+    number_of_subscribers(sys.argv[1])
