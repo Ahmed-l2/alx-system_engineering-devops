5151ae2a14d6e1e5f6d7fa43b13c9ae77320d51b
Ahmed <ahmed_l2@yahoo.com>
2024-05-07 18:41:00 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
 """Script that fetches Number of subscribers for a subreddit"""
-
-from sys import argv
 import requests
 
 
-def fetchSubs():
+def number_of_subscribers(subreddit):
     """Fetches subreddit subcount"""
-    if len(argv) < 2:
-        return print("Usage: {} <subreddit>".format(argv[0]))
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(argv[1])
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     response = requests.get(url, allow_redirects=False)
 
     if response.status_code == 200:
-        print(response.json()['data']['subscribers'])
+        return response.json()['data']['subscribers']
     else:
-        return print(0)
-
-
-if __name__ == '__main__':
-    fetchSubs()
+        return 0
