4fd7e72e531eb66e619e31987e64659d1ba6c270
Ahmed <ahmed_l2@yahoo.com>
2024-05-08 21:46:47 +0300

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 # Puppet manifest to replace occurrences of "phpp" with "php" in /var/www/html/wp-settings.php
 
-file_line { 'replace_phpp_with_php':
-  ensure => present,
-  path   => '/var/www/html/wp-settings.php',
-  line   => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  match  => '^.*phpp.*$',
+exec { 'replace_phpp_with_php':
+  command => "sed -i 's/phpp/php/g' '/var/www/html/wp-settings.php',
+  path    => '/bin','/usr/bin'
 }
